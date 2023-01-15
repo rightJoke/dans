acc15e608dcc36f5e01cf3e4fa4c3252d847d930
erdem abat <erdemabat@hotmail.com>
2023-01-15 22:50:01 +0300

    SliderViewComponent

---
+++ PetShop/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetShop.WebUI.ViewComponents
+{
+    public class SliderViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
